ff53d8f91e54f06c745250b952f938f3f8d01937
RegineGV <contact.rr2006@gmail.com>
2026-02-04 19:50:51 +0700

    week01: (tugas 1) clean up unused args and comments

--- src/oop_00000105852_RegineGabrielleViola/week01/CircleCalculator.kt
+++ src/oop_00000105852_RegineGabrielleViola/week01/CircleCalculator.kt
@@ -1,16 +1,13 @@
 package oop_00000105852_RegineGabrielleViola.week01
 
 fun main() {
-    // Checkpoint 7: Val & String Templates
     val radius = 7.0
     val pi = 3.14
     val area = pi * radius * radius
 
     println("Radius: $radius, Area: $area")
 
-    // Checkpoint 8: Panggil fungsi expression body
     println(checkSize(area))
 }
 
-// Checkpoint 8: Expression Body Function (mengembalikan String)
 fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
