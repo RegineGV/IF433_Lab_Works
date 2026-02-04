6432917a9ae9c83378c9d5708182cbb3c5e689da
RegineGV <contact.rr2006@gmail.com>
2026-02-04 20:13:18 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000105852_RegineGabrielleViola/week01/GameStore.kt
+++ src/oop_00000105852_RegineGabrielleViola/week01/GameStore.kt
@@ -3,4 +3,9 @@ package oop_00000105852_RegineGabrielleViola.week01
 fun main() {
     val title = "ENCHANTED RING"
     val price = 600000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 }
+
+fun calculateDiscount(price: Int) = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
