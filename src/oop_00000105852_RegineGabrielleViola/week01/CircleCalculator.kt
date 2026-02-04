6b2aea60e1fe1eeee35e595a86b8c295d8a9f470
RegineGV <contact.rr2006@gmail.com>
2026-02-04 19:50:17 +0700

    week01: (tugas 1) convert checkSize to expression body function

--- src/oop_00000105852_RegineGabrielleViola/week01/CircleCalculator.kt
+++ src/oop_00000105852_RegineGabrielleViola/week01/CircleCalculator.kt
@@ -1,19 +1,16 @@
 package oop_00000105852_RegineGabrielleViola.week01
 
-fun main(args: Array<String>) {
+fun main() {
+    // Checkpoint 7: Val & String Templates
     val radius = 7.0
     val pi = 3.14
     val area = pi * radius * radius
 
     println("Radius: $radius, Area: $area")
 
-    checkSize(area)
+    // Checkpoint 8: Panggil fungsi expression body
+    println(checkSize(area))
 }
 
-fun checkSize(area: Double) {
-    if (area > 100) {
-        println("This is a Big Circle")
-    } else {
-        println("This is a Small Circle")
-    }
-}
+// Checkpoint 8: Expression Body Function (mengembalikan String)
+fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
