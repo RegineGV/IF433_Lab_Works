c4f14127bbb1ccc1c665a9c7fbcfd98d11572355
RegineGV <contact.rr2006@gmail.com>
2026-02-04 19:34:16 +0700

    week01: implement null safety check

--- src/oop_00000105852_RegineGabrielleViola/week01/GradingSystem.kt
+++ src/oop_00000105852_RegineGabrielleViola/week01/GradingSystem.kt
@@ -1,10 +1,12 @@
 package oop_00000105852_RegineGabrielleViola.week01
 
 fun main() {
+    // Checkpoint 2: Refactor variables (val & String template)
     val name = "John Thor"
     val score = 80
     println("Nama: $name, Nilai: $score")
 
+    // Checkpoint 3: Grading Logic (when expression)
     val grade = when (score) {
         in 90..100 -> "A"
         in 80..89 -> "B"
@@ -13,7 +15,15 @@ fun main() {
     }
     println("Grade kamu: $grade")
 
+    // Checkpoint 4: Call calculateStatus
     println("Status: ${calculateStatus(score)}")
+
+    // Checkpoint 5: Null Safety
+    val studentId: String? = null
+    // Elvis operator: jika null, return 0
+    val idLength = studentId?.length ?: 0
+    println("Panjang ID: $idLength")
 }
 
+// Checkpoint 4: Function expression body (di luar main)
 fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
