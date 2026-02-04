9c5c47f6ede2f868eb67c14bf5daa784c21d1255
RegineGV <contact.rr2006@gmail.com>
2026-02-04 20:15:10 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000105852_RegineGabrielleViola/week01/GameStore.kt
+++ src/oop_00000105852_RegineGabrielleViola/week01/GameStore.kt
@@ -1,11 +1,18 @@
 package oop_00000105852_RegineGabrielleViola.week01
 
 fun main() {
-    val title = "ENCHANTED RING"
+    val title = "ENCHANTED Ring"
     val price = 600000
-
     val discount = calculateDiscount(price)
     val finalPrice = price - discount
+
+    printReceipt(title = title, finalPrice = finalPrice)
 }
 
 fun calculateDiscount(price: Int) = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("=== STRUK PEMBELIAN ===")
+    println("Game: $title")
+    println("Bayar: Rp $finalPrice")
+}
