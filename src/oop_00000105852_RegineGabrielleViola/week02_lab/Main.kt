54bf6edd197d30c65a7bcfe9dcac49335ba94aae
RegineGV <contact.rr2006@gmail.com>
2026-02-11 11:27:33 +0700

    week02: implement selection logic for constructors

--- src/oop_00000105852_RegineGabrielleViola/week02_lab/Main.kt
+++ src/oop_00000105852_RegineGabrielleViola/week02_lab/Main.kt
@@ -11,17 +11,34 @@ fun main() {
 
     print("Masukkan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
-    scanner.nextLine() // Bersihkan buffer newline (Penyakit klasik Scanner)
+    scanner.nextLine() // Bersihkan buffer newline
 
-    // Validasi di sisi pemanggil (Main)
+    // Validasi NIM
     if (nim.length != 5) {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
     } else {
-        print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        if (scanner.hasNextInt()) {
+            val type = scanner.nextInt()
+            scanner.nextLine()
 
-        // Instansiasi Objek karena data sudah aman
-        val s1 = Student(name, nim, major)
-        println("Status: Pendaftaran Selesai.")
+            if (type == 1) {
+                print("Masukkan Jurusan: ")
+                val major = scanner.nextLine()
+
+                val s1 = Student(name, nim, major) // Primary Constructor [cite: 83-84]
+                println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+
+            } else if (type == 2) {
+                val s2 = Student(name, nim) // Secondary Constructor [cite: 87]
+                println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+
+            } else {
+                println("Pilihan ngawur, pendaftaran batal!")
+            }
+        } else {
+            println("Input harus angka!")
+        }
     }
+    println("Status: Pendaftaran Selesai.")
 }
