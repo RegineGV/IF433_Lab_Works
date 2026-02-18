db93510f58f2317c1bed934b4d40e684bae0117e
RegineGV <contact.rr2006@gmail.com>
2026-02-18 15:00:56 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000105852_RegineGabrielleViola/week03_lab/Employee.kt
+++ src/oop_00000105852_RegineGabrielleViola/week03_lab/Employee.kt
@@ -2,4 +2,8 @@ package oop_00000105852_RegineGabrielleViola.week03_lab
 
 class Employee (val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            this.salary = value
+        }
 }
---
+++ src/oop_00000105852_RegineGabrielleViola/week03_lab/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000105852_RegineGabrielleViola.week03_lab
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
