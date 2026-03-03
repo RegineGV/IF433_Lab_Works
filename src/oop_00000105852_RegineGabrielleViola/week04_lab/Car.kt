8736144c8d213ed2ec3ee7ea433d7881fe8bdcda
RegineGV <contact.rr2006@gmail.com>
2026-03-04 00:00:35 +0700

    week04: create Car class extending Vehincle

---
+++ src/oop_00000105852_RegineGabrielleViola/week04_lab/Car.kt
@@ -0,0 +1,7 @@
+package oop_00000105852_RegineGabrielleViola.week04_lab
+
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
+    fun openTrunk() {
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    }
+}
