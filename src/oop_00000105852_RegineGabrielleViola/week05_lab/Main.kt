ddd26665cdf10034d7079aebc68c2b55a8da6382
RegineGV <contact.rr2006@gmail.com>
2026-03-04 10:53:21 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_00000105852_RegineGabrielleViola/week05_lab/Main.kt
@@ -0,0 +1,24 @@
+package oop_00000105852_RegineGabrielleViola.week05_lab
+
+fun main() {
+    val dosen1 = Dosen("Pak Alex", "0123456")
+    val admin1 = Admin("Bu Siti")
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIVITAS PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("-----------------------------------")
+    }
+}
