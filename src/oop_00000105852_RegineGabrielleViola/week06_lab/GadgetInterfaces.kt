972262cb24b8251247cc51e6fbd23419f99eb212
RegineGV <contact.rr2006@gmail.com>
2026-03-11 10:50:44 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000105852_RegineGabrielleViola/week06_lab/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000105852_RegineGabrielleViola.week06_lab
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
