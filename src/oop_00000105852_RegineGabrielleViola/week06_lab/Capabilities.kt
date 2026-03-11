f226855affb41ed3dea89dff25b73a990d03ddda
RegineGV <contact.rr2006@gmail.com>
2026-03-11 10:30:42 +0700

    week06: create compound interfaces

---
+++ src/oop_00000105852_RegineGabrielleViola/week06_lab/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_00000105852_RegineGabrielleViola.week06_lab
+
+// Interface -> Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
