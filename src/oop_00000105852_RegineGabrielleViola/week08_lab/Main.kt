e19e685775553507ae1dd976631610688728b9f6
RegineGV <contact.rr2006@gmail.com>
2026-04-15 10:39:49 +0700

    week08: test safe casting in main

--- src/oop_00000105852_RegineGabrielleViola/week08_lab/Main.kt
+++ src/oop_00000105852_RegineGabrielleViola/week08_lab/Main.kt
@@ -32,4 +32,6 @@ fun main() {
     val someObject: Any = 100
     val safeString = someObject as? String ?: "Unknown String"
     println("Hasil cast fallback: $safeString")
+
+    //coba main.kt
 }
