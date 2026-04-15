4ccb0dae0063120d064b2b6072ef8290667045da
RegineGV <contact.rr2006@gmail.com>
2026-04-15 10:55:10 +0700

    week08: simulate unit test failure using git add .

--- src/oop_00000105852_RegineGabrielleViola/week08_lab/UserTest.kt
+++ src/oop_00000105852_RegineGabrielleViola/week08_lab/UserTest.kt
@@ -1,4 +1,21 @@
 package oop_00000105852_RegineGabrielleViola.week08_lab
 
-class UserTest {
+object DatabaseMock {
+    fun findUser(id: Int): UserProfile? {
+        // Mengembalikan User jika ID adalah 1, selain itu null [cite: 170]
+        return if (id == 1) UserProfile("TestUser", "test@test.com") else null
+    }
+
+    fun runMockUnitTest() {
+        println("\n=== RUNNING UNIT TEST ===")
+        val testUser = DatabaseMock.findUser(1)
+
+        // Di Unit Testing, jika user null, kita HARAP testnya gagal (crash).
+        // Maka penggunaan !! sangat lazim di file Testing.
+        val initial = testUser!!.name.substring(0, 1)
+
+        // check() adalah fungsi bawaan Kotlin untuk assertion
+        check(initial == "T") { "Test Failed! Initial is wrong." }
+        println("Test Passed: Initial is T")
+    }
 }
