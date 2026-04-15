15214aaed16c0eb13b83582aa306c4fda1c76576
RegineGV <contact.rr2006@gmail.com>
2026-04-15 10:43:44 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000105852_RegineGabrielleViola/week08_lab/LegacyJavaAPI.java
@@ -0,0 +1,7 @@
+package oop_00000105852_RegineGabrielleViola.week08_lab;
+
+public class LegacyJavaAPI {
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
