1d70d2e24c23642f07f095aa0bef07333bc8158a
RegineGV <contact.rr2006@gmail.com>
2026-04-15 10:20:52 +0700

    week08: implement safe call with let block

--- src/oop_00000105852_RegineGabrielleViola/week08_lab/Main.kt
+++ src/oop_00000105852_RegineGabrielleViola/week08_lab/Main.kt
@@ -7,4 +7,15 @@ fun main() {
     // Rantai Safe Calls: Berhenti dan return null jika ada yang null di tengah
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
     println("Tujuan pengiriman: $destination")
+
+    println("\n=== TEST LET BLOCK ===")
+    val validOrder = Order(null, 250000)
+
+    // .let memastikan blok hanya jalan jika tidak null
+    val receipt = validOrder.totalPrice?.let { price ->
+        val tax = price * 0.11
+        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+
+    println(receipt)
 }
