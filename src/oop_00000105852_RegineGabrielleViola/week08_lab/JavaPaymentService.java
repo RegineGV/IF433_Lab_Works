ad7812ad124448c0905fef0c3a9c01c7034441cf
RegineGV <contact.rr2006@gmail.com>
2026-04-15 11:13:52 +0700

    week08: (task) create Java Payment Service for payment interop

---
+++ src/oop_00000105852_RegineGabrielleViola/week08_lab/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000105852_RegineGabrielleViola.week08_lab;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
