d97adff366c160f873176e6ceefc089bd5bfd44d
RegineGV <contact.rr2006@gmail.com>
2026-04-15 10:19:43 +0700

    week08: implement chained safe calls and elvis

--- src/oop_00000105852_RegineGabrielleViola/week08_lab/Main.kt
+++ src/oop_00000105852_RegineGabrielleViola/week08_lab/Main.kt
@@ -1,4 +1,10 @@
 package oop_00000105852_RegineGabrielleViola.week08_lab
 
-class Main {
+fun main() {
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+
+    // Rantai Safe Calls: Berhenti dan return null jika ada yang null di tengah
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
+    println("Tujuan pengiriman: $destination")
 }
