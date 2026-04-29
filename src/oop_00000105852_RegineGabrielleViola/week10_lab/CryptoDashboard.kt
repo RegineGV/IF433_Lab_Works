3a352bd2d0d604be4d81e4f07f00d1b76eadc40b
RegineGV <contact.rr2006@gmail.com>
2026-04-29 10:50:43 +0700

    week10: (task) populate coin repository

--- src/oop_00000105852_RegineGabrielleViola/week10_lab/CryptoDashboard.kt
+++ src/oop_00000105852_RegineGabrielleViola/week10_lab/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000105852_RegineGabrielleViola.week10_lab
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 3.2))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
