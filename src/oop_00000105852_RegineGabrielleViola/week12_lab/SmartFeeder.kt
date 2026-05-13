937f53387c1305e95619d92ae73b53b0192567e2
RegineGV <contact.rr2006@gmail.com>
2026-05-13 10:21:44 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000105852_RegineGabrielleViola/week12_lab/SmartFeeder.kt
@@ -0,0 +1,6 @@
+package oop_00000105852_RegineGabrielleViola.week12_lab
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+    return availableGram // placeholder, akan dilengkapi checkpoint berikut
+}
