66066c7bad27ea0dbbce2d8f6b9b53647171bd89
RegineGV <contact.rr2006@gmail.com>
2026-05-20 22:22:03 +0700

    week13: create Student model

---
+++ src/oop_00000105852_RegineGabrielleViola/week13_lab/StudentSerialization.kt
@@ -0,0 +1,6 @@
+package oop_00000105852_RegineGabrielleViola.week13_lab
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(val name: String, val age: Int, val gpa: Double)
