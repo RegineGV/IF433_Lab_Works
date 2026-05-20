a4e4b12d1196ad3ada5088a252d51efb4189cfbb
RegineGV <contact.rr2006@gmail.com>
2026-05-20 22:25:50 +0700

    week13: (task) implement loadTrades with FileNotFoundException handling

--- src/oop_00000105852_RegineGabrielleViola/week13_lab/CryptoTradeLogger.kt
+++ src/oop_00000105852_RegineGabrielleViola/week13_lab/CryptoTradeLogger.kt
@@ -14,17 +14,6 @@ data class TradeRecord(
 fun TradeRecord.toCsv(): String = "$id,$symbol,$type,$margin,$pnl"
 
 fun fromCsvTrade(line: String): TradeRecord? {
-    val parts = line.split(",")
-    return TradeRecord(
-        id = parts[0].toInt(),
-        symbol = parts[1],
-        type = parts[2],
-        margin = parts[3].toDouble(),
-        pnl = parts[4].toDouble()
-    )
-}
-
-fun fromCsvTrade(line: String): TradeRecord? {
     return try {
         val parts = line.split(",")
         TradeRecord(
@@ -45,5 +34,14 @@ fun saveTrades(trades: List<TradeRecord>, path: String) {
         trades.forEach { trade ->
             writer.println(trade.toCsv())
         }
+    }
+}
+
+fun loadTrades(path: String): List<TradeRecord> {
+    return try {
+        File(path).readLines().mapNotNull { fromCsvTrade(it) }
+    } catch (e: FileNotFoundException) {
+        println("Error: File tidak ditemukan!")
+        emptyList()
     }
 }
